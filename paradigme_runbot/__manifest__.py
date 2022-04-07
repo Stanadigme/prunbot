d319a3ec32bbf99d6aed5dd73c646f2b292a30bf
stan@desktop <stanley@paradigme.io>
2022-04-07 16:25:42 -0400

    220407

---
+++ paradigme_runbot/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"Runbot Paradigme",
+    'summary': "Données Paradigme",
+    'depends':['runbot'],
+    'auto_install':True,
+    'data':[
+        'repositories.xml'
+    ]
+}
